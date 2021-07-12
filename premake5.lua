d052dbb86d328935b9e35ebf66cd1c0cc31b22f3
Eugene Lebedev <EugLeb2016@gmail.com>
2021-07-13 01:05:54 +0300

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -1,25 +1,50 @@
 project "GLFW"
-  kind "StaticLib"
-  language "C"
-  
-  targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-  objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+	kind "StaticLib"
+	language "C"
+	staticruntime "off"
 
-  files
-  {
-    "include/GLFW/glfw3.h",
-    "include/GLFW/glfw3native.h",
-    "src/glfw_config.h",
-    "src/context.c",
-    "src/init.c",
-    "src/input.c",
-    "src/monitor.c",
-    "src/vulkan.c",
-    "src/window.c"
-  }
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"include/GLFW/glfw3.h",
+		"include/GLFW/glfw3native.h",
+		"src/glfw_config.h",
+		"src/context.c",
+		"src/init.c",
+		"src/input.c",
+		"src/monitor.c",
+		"src/vulkan.c",
+		"src/window.c"
+	}
+	filter "system:linux"
+		pic "On"
+
+		systemversion "latest"
+		
+		files
+		{
+			"src/x11_init.c",
+			"src/x11_monitor.c",
+			"src/x11_window.c",
+			"src/xkb_unicode.c",
+			"src/posix_time.c",
+			"src/posix_thread.c",
+			"src/glx_context.c",
+			"src/egl_context.c",
+			"src/osmesa_context.c",
+			"src/linux_joystick.c"
+		}
+
+		defines
+		{
+			"_GLFW_X11"
+		}
+
 	filter "system:windows"
 		systemversion "latest"
-		staticruntime "On"
+
 		files
 		{
 			"src/win32_init.c",
